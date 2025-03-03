e7ebc71cae5f97a951bdf44232902c88cb10a1e6
Onkar Karale <onkarkarale4@gmail.com>
2025-03-03 15:29:33 +0530

    Updated Code and made connection to database

--- SnapCart-Backend/src/app.js
+++ SnapCart-Backend/src/app.js
@@ -7,5 +7,15 @@ app.use(express.json());
 
 app.use(productRoutes)
 
+// Global Express Error Handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'Internal Server Error' } = err;
+  res.status(statusCode).json({
+    status: 'error',
+    statusCode,
+    message
+  });
+})
+
 export default app
  
