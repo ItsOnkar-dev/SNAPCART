03b5ea8f6b48ee8146fd6950b3356c4f16616980
Onkar Karale <onkarkarale4@gmail.com>
2025-03-30 18:42:25 +0530

    Updated previous code

--- SnapCart-Backend/src/Models/User.js
+++ SnapCart-Backend/src/Models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Seller', 'Buyer', 'Admin']
+    enum: ['Seller', 'Buyer', 'Admin'],
   },
 }, { timestamps: true, versionKey: false });
 
